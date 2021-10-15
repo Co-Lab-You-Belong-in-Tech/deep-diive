dc6b4a5c138598a44b195d4bcddd8fd7481017e4
jerriefeng <jerrie.feng@mail.utoronto.ca>
2021-10-15 15:03:48 -0400

    linking pages and stuff

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import Routes from "./routes/Routes";
+import Landing from "./app/pages/Landing/Landing";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
--- my-app/src/routes/Routes.js
+++ my-app/src/routes/Routes.js
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GameView from "../app/pages/GameView/GameView";
 import Home from "../app/pages/Home";
+import Landing from "../app/pages/Landing/Landing";
+import Onboarding from "../app/pages/Onboarding/Onboarding";
+
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/landing" exact component={Landing} />
+        <Route path="/onboarding" exact component={Onboarding} />
         <Route path="/game" exact component={GameView} />
       </Switch>
     </Router>
