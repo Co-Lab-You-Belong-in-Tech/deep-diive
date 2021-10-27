42a9ae15e6aef933286e1cbdbcbdd23c7a5b58ca
dimola7 <didiogidan@gmail.com>
2021-10-27 21:02:50 +0100

    added rules modal

---
+++ client/src/app/components/Rules/Rules.jsx
@@ -0,0 +1,18 @@
+import beach from "../../assets/gifs/beach-waves-2.gif";
+import oceanWaves from "../../assets/audio/ocean-wave-2.mp3";
+import ruleStyles from "./Rules.module.css";
+
+const Rules = ({hide}) => {
+    return (
+        <div className={ruleStyles.modal}>
+                    <p>Press <span>start</span> when <br /> everyone has arrived.</p>
+                    <img src={beach} alt="" />
+                    <button onClick={hide}>start</button>
+                    <audio autoPlay loop >
+                        <source src={oceanWaves} type="audio/mpeg" />
+                    </audio>
+                </div>
+    )
+}
+
+export default Rules;
