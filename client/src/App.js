8f4dbad9c14a4416d28644c1d632f9878292a39e
jerriefeng <jerrie.feng@mail.utoronto.ca>
2021-11-04 12:27:15 -0400

    mobile

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import Routes from "./app/routes/Routes";
+import { isMobile } from 'react-device-detect';
 import "./App.css";
+import Mobile from "./app/pages/Mobile/Mobile";
 
 function App() {
 
   return (
-    <Routes />
-  );
+    isMobile ? <Mobile/> : <Routes />
+  ); 
+  
 }
 
 export default App;
