833a38070f1d4ef242daa64901b652ddadb0e2c9
dimola7 <didiogidan@gmail.com>
2022-01-18 23:49:09 +0100

    setup another server

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    res.send("welcome to deepdiive api");
+  });
+
 const server = http.createServer(app);
 
 const io = socketio(server, {
