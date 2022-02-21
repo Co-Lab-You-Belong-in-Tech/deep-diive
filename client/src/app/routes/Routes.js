76b97387f11ebbe58704ff4400c578d8e423f8c3
dimola7 <didiogidan@gmail.com>
2022-02-21 15:34:25 +0100

    completed google analytics implementation

--- client/src/app/routes/Routes.js
+++ client/src/app/routes/Routes.js
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { usePathname } from "../hooks/usePathname";
 import GameView from "../pages/GameView/GameView";
 import Landing from "../pages/Landing/Landing";
 import Onboarding from "../pages/Onboarding/Onboarding";
@@ -7,10 +9,38 @@ import Feedback from "../pages/Feedback/Feedback";
 import Instruction from "../pages/Instruction/Instruction";
 import GameStart from "../pages/GameStart/GameStart";
 import InvitedInstructions from "../pages/InvitedInstructions/InvitedInstructions";
+import ReactGA from 'react-ga';
+import InitializeReactGA from "../helpers/googleAnalytics";
+
+function usePageViews() {
+	let pathname = usePathname();
+	useEffect(() => {
+		InitializeReactGA(ReactGA);
+		ReactGA.set({ page: pathname });
+		ReactGA.pageview(pathname);
+    // console.log(pathname);
+	}, [pathname]);
+}
 
 const Routers = () => {
+
+  const setGA = () => {
+    ReactGA.initialize('G-QGGY6NY4E9', {
+      debug: true,
+      titleCase: false,
+      gaOptions: {
+        userId: 123,
+        siteSpeedSampleRate: 100
+      }
+    });
+  };
+
+  useEffect(() => {
+    setGA();
+  }, [])
+
+  usePageViews();
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/onboarding" element={<Onboarding />} />
@@ -27,7 +57,6 @@ const Routers = () => {
         <Route path="/start/:gameId" element={<GameStart />} />
         <Route path="/feedback" element={<Feedback />} />
       </Routes>
-    </Router>
   );
 };
 
